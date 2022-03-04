a87c4a921fbaf563a84108f48288d82535ffa421
Doug Hennig <doug@doughennig.com>
2022-03-04 15:16:29 -0600

    Updated version file

--- Thor/Source/thorversion.h
+++ Thor/Source/thorversion.h
@@ -1,8 +1,8 @@
-* 1.43
+* 1.44
 
-*** DH 2021-12-28: changed version information
-#Define cnVersion         1.43
-#Define cdVersionDate     December 28, 2021
-#Define	ccThorInternalVERSION     [Thor - 1.43 - December 28, 2021- 20211228]
-#Define	ccThorVERSION     [Thor - 1.43 - December 28, 2021]
+*** DH 2022-03-04: changed version information
+#Define cnVersion         1.44
+#Define cdVersionDate     March 4, 2022
+#Define	ccThorInternalVERSION     [Thor - 1.44 - March 4, 2022- 20220304]
+#Define	ccThorVERSION     [Thor - 1.44 - March 4, 2022]
 #Define	ccThorVERSIONFILE [ThorVersion.txt]
