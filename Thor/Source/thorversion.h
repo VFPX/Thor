8bc3ca9ba11582220c106999de914eb41410c8e2
Jim Nelson <JimRNelson@GMail.Com>
2022-11-20 17:11:22 -0700

    Can now delete or move separators in menus

--- Thor/Source/thorversion.h
+++ Thor/Source/thorversion.h
@@ -1,7 +1,7 @@
 * 1.45
 
-#Define cnVersion         1.45
-#Define cdVersionDate     March 5, 2022
-#Define	ccThorInternalVERSION     [Thor - 1.45 - March 5, 2022- 20220305]
-#Define	ccThorVERSION     [Thor - 1.45 - March 5, 2022]
+#Define cnVersion         1.45.01
+#Define cdVersionDate     November 20, 2022
+#Define	ccThorInternalVERSION     [Thor - 1.45.01 - November 20, 2022- 20221120]
+#Define	ccThorVERSION     [Thor - 1.45.01 - November 20, 2022]
 #Define	ccThorVERSIONFILE [ThorVersion.txt]
