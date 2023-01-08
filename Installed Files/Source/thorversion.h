4e1de9fac5895d387b351edc50cc552102c09651
Jim Nelson <JimRNelson@GMail.Com>
2023-01-08 07:25:56 -0700

    correct problem with version number (doh)

--- Installed Files/Source/thorversion.h
+++ Installed Files/Source/thorversion.h
@@ -2,6 +2,6 @@
 
 #Define cnVersion         1.45.04
 #Define cdVersionDate     January 6, 2023
-#Define	ccThorInternalVERSION     [Thor - 1.45.04 - January 6, 2023]
+#Define	ccThorInternalVERSION     'Thor - 1.45.04 - January 6, 2023- 20230106'
 #Define	ccThorVERSION     [Thor - 1.45.04 - January 6, 2023]
 #Define	ccThorVERSIONFILE [ThorVersion.txt]
