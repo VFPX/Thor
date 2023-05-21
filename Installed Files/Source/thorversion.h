9baf3f3c4560d31564769122e3b7aaa3eadc916b
Jim Nelson <JimRNelson@GMail.Com>
2023-05-21 15:11:20 -0600

    starting 1.45.22

--- Installed Files/Source/thorversion.h
+++ Installed Files/Source/thorversion.h
@@ -1,5 +1,5 @@
-#Define cnVersion         			1.45.21
-#Define cdVersionDate     			May 20, 2023
-#Define	ccThorInternalVERSION     	'Thor - 1.45.21 - May 20, 2023- 20230520'
-#Define	ccThorVERSION     			[Thor - 1.45.21 - May 20, 2023]
+#Define cnVersion         			1.45.22
+#Define cdVersionDate     			May 27, 2023
+#Define	ccThorInternalVERSION     	'Thor - 1.45.22 - May 27, 2023- 20230527'
+#Define	ccThorVERSION     			[Thor - 1.45.22 - May 27, 2023]
 #Define	ccThorVERSIONFILE 			[ThorVersion.txt]
