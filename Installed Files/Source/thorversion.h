857c0a77d43af4b5438b551027079afd8fe5f71f
Jim Nelson <JimRNelson@GMail.Com>
2023-08-18 16:22:23 -0600

    Version update, including Change Logs

--- Installed Files/Source/thorversion.h
+++ Installed Files/Source/thorversion.h
@@ -1,5 +1,5 @@
-#Define cnVersion         			1.46
-#Define cdVersionDate     			August 11, 2023
-#Define	ccThorInternalVERSION     	'Thor - 1.46 - August 11, 2023- 20230811'
-#Define	ccThorVERSION     			[Thor - 1.46 - August 11, 2023]
+#Define cnVersion         			1.46.01
+#Define cdVersionDate     			August 19, 2023
+#Define	ccThorInternalVERSION     	'Thor - 1.46.01 - August 19, 2023- 20230819'
+#Define	ccThorVERSION     			[Thor - 1.46.01 - August 19, 2023]
 #Define	ccThorVERSIONFILE 			[ThorVersion.txt]
