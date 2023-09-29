cc8059fe64c610c57565aa725560751e3cec60de
Jim Nelson <JimRNelson@GMail.Com>
2023-09-29 13:24:57 -0600

    Verison 1.45.06: New tool: "Tool Manager"

--- Installed Files/Source/thorversion.h
+++ Installed Files/Source/thorversion.h
@@ -1,5 +1,5 @@
-#Define cnVersion         			1.46.05
-#Define cdVersionDate     			September 28, 2023
-#Define	ccThorInternalVERSION     	'Thor - 1.46.05 - September 28, 2023- 20230928'
-#Define	ccThorVERSION     			[Thor - 1.46.05 - September 28, 2023]
+#Define cnVersion         			1.46.06
+#Define cdVersionDate     			September 29, 2023
+#Define	ccThorInternalVERSION     	'Thor - 1.46.06 - September 29, 2023- 20230929'
+#Define	ccThorVERSION     			[Thor - 1.46.06 - September 29, 2023]
 #Define	ccThorVERSIONFILE 			[ThorVersion.txt]
