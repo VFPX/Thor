859505929fcc3ba02009da2e8eba266c03a7d1eb
Jim Nelson <JimRNelson@GMail.Com>
2023-10-28 18:09:02 -0600

    Thor 1.46.10 * New table "VFPHotKeys" with list of all VFP shortcut keys:     * User warned about possible conflicts when assigning hot keys     * Page "Hot Key Assignments" in Thor Configuration form has new column showing possible conflicts * Remove HotKeyDefinitions, erroneously added in 1.46.08 * New optional font size for Thor menus * New tool "Force Thor Update"

--- Installed Files/Source/thorversion.h
+++ Installed Files/Source/thorversion.h
@@ -1,5 +1,5 @@
 #Define cnVersion         			1.46.10
-#Define cdVersionDate     			October 25, 2023
-#Define	ccThorInternalVERSION     	'Thor - 1.46.10 - October 25, 2023- 20231025'
-#Define	ccThorVERSION     			[Thor - 1.46.10 - October 25, 2023]
+#Define cdVersionDate     			October 28, 2023
+#Define	ccThorInternalVERSION     	'Thor - 1.46.10 - October 28, 2023- 20231028'
+#Define	ccThorVERSION     			[Thor - 1.46.10 - October 28, 2023]
 #Define	ccThorVERSIONFILE 			[ThorVersion.txt]
