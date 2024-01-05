cbe102ebff54fd44c3221ed21fddcf41396bc388
Jim Nelson <JimRNelson@GMail.Com>
2024-01-05 16:23:54 -0700

    Thor 1.47.01 - Released 2024-01-05 * Fixed bug when opening Thor Configuration screen.

--- Installed Files/Source/thorversion.h
+++ Installed Files/Source/thorversion.h
@@ -1,5 +1,5 @@
-#Define cnVersion         			1.47
-#Define cdVersionDate     			January 4, 2024
-#Define	ccThorInternalVERSION     	'Thor - 1.47 - January 4 2024- 20240104'
-#Define	ccThorVERSION     			[Thor - 1.47 - January 4, 2024]
+#Define cnVersion         			1.47.01
+#Define cdVersionDate     			January 5, 2024
+#Define	ccThorInternalVERSION     	'Thor - 1.47.01 - January 5 2024- 20240105'
+#Define	ccThorVERSION     			[Thor - 1.47.01 - January 5, 2024]
 #Define	ccThorVERSIONFILE 			[ThorVersion.txt]
